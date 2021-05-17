de57ed5aedc4d5b7169ba77f0e80ab6b8da9266d
Artjom_Islyamov_notebook <pixeltom@yandex.ru>
2021-05-17 22:53:02 +0300

    1 Task

---
+++ src/main/java/ru/geekbrains/artjom_islyamov/HomeWorkLesson2.java
@@ -0,0 +1,19 @@
+package ru.geekbrains.artjom_islyamov;
+
+import com.sun.source.doctree.SummaryTree;
+
+public class HomeWorkLesson2 {
+    public static void main(String[] args) {
+       System.out.println(SumOfTwoNumber(10,0));
+
+        System.out.println();
+
+        }
+    public static boolean SumOfTwoNumber (int a,int b){
+        if((a+b)>=10 && (a+b)<=20)
+        return true;
+        else return false;
+
+        }}
+
+
