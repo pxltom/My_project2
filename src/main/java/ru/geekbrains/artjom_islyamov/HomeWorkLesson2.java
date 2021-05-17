8224deee07d8a575f262d4186e00b3a41a9cfd8b
Artjom_Islyamov_notebook <pixeltom@yandex.ru>
2021-05-18 01:31:37 +0300

    4 tast,one left

--- src/main/java/ru/geekbrains/artjom_islyamov/HomeWorkLesson2.java
+++ src/main/java/ru/geekbrains/artjom_islyamov/HomeWorkLesson2.java
@@ -1,16 +1,28 @@
 package ru.geekbrains.artjom_islyamov;
 
-import com.sun.source.doctree.SummaryTree;
-
 public class HomeWorkLesson2 {
     public static void main (String[] args) {
-        System.out.println(SumOfTwoNumber(10, 0));
+        System.out.println(sumOfTwoNumber(10, 30));
+        System.out.println(falseIfNegativeAndTrueIfPositive(-15));
+        positiveOrNegativeNumber(-20);
+        stringLoopTimes("Java...Rulezzz",5);
+    }
+    public static boolean sumOfTwoNumber (int a, int b) {
+        return (a + b) >= 10 && (a + b) <= 20;
+    }
+    public static void positiveOrNegativeNumber (int a) {
+        if (a > 0) {
+            System.out.println("Положительное");
+        } else System.out.println("Отрицательное");
+    }
+    public static boolean falseIfNegativeAndTrueIfPositive (int a) {
+        return a < 0 ? true : false;
+    }
+    public static void  stringLoopTimes(String a, int b){
+        for (int i = 0; i < b; i++)
+        System.out.println(a);
+
     }
 
-    public static boolean SumOfTwoNumber (int a, int b) {
-        if ((a + b) >= 10 && (a + b) <= 20) {
-            return true;
-        } else return false;
-    }
 }
 
