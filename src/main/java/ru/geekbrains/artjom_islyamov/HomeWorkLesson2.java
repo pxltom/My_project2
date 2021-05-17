e7da5740a35b514feb1fce93fe95a18bea45ca29
Artjom_Islyamov_notebook <pixeltom@yandex.ru>
2021-05-18 02:40:07 +0300

    almost all tasks is done

--- src/main/java/ru/geekbrains/artjom_islyamov/HomeWorkLesson2.java
+++ src/main/java/ru/geekbrains/artjom_islyamov/HomeWorkLesson2.java
@@ -5,24 +5,36 @@ public class HomeWorkLesson2 {
         System.out.println(sumOfTwoNumber(10, 30));
         System.out.println(falseIfNegativeAndTrueIfPositive(-15));
         positiveOrNegativeNumber(-20);
-        stringLoopTimes("Java...Rulezzz",5);
+        stringLoopTimes("Java...Rulezzz", 5);
+        System.out.println(leapYearsOrNot(2003));
     }
+
     public static boolean sumOfTwoNumber (int a, int b) {
         return (a + b) >= 10 && (a + b) <= 20;
     }
+
     public static void positiveOrNegativeNumber (int a) {
         if (a > 0) {
             System.out.println("Положительное");
         } else System.out.println("Отрицательное");
     }
+
     public static boolean falseIfNegativeAndTrueIfPositive (int a) {
         return a < 0 ? true : false;
     }
-    public static void  stringLoopTimes(String a, int b){
+
+    public static void stringLoopTimes (String a, int b) {
         for (int i = 0; i < b; i++)
-        System.out.println(a);
+            System.out.println(a);
 
     }
 
+    /*  к сожалению я посмотрел последнее задание в интернете
+    в моей голове были мысли о создании огромного массива, метода сканер и цикла,
+    а всё оказалось элементарно и просто, мозг всё услоднял
+      */
+    public static boolean leapYearsOrNot (int b) {
+        return (b % 4 == 0 && b % 100 != 0 || b % 400 == 0);
+    }
 }
 
